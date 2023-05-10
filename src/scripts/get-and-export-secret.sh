c4cfc58e78b5fc39f39b9957cef59e29bfe85819
yxfeng <yxfeng@thoughtworks.com>
2023-05-10 17:51:08 +0800

    export secrets from aws secret manager

---
+++ src/scripts/get-and-export-secret.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+secretName=$1
+secretKey=$2
+exportedVariable=$3
+
+secretValue=`aws secretsmanager get-secret-value --secret-id ${secretName} | jq --raw-output '.SecretString' | jq -r .${secretKey}`
+export exportedVariable=$secretValue
+echo $exportedVariable
